d97edc4b7472100c69c27481c50e70d2266fbbbb
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2025-09-16 23:36:56 +0600

    Update apps.py

--- chat/apps.py
+++ chat/apps.py
@@ -1,6 +1,7 @@
-# ./chat/apps.py
-from django.apps import AppConfig
+    # ./chat/apps.py
+    from django.apps import AppConfig
 
-class ChatConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chat'
+    class ChatConfig(AppConfig):
+        default_auto_field = 'django.db.models.BigAutoField'
+        name = 'chat'
+        verbose_name = "Chat Application"
